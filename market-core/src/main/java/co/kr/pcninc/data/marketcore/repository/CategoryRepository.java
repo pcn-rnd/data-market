2d69871431ff8ea83d0fd00fc51c7ac9fe7ae020
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:48:21 +0900

    카테고리 jpa repo interface 정의

---
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package co.kr.pcninc.data.marketcore.repository;
+
+import co.kr.pcninc.data.marketcore.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
