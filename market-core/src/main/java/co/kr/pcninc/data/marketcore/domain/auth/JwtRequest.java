9de516841a11dd95b016ab71dec58cb570c58e4e
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:45:50 +0900

    JwtRequest, Response domain 정의

--- market-core/src/main/java/co/kr/pcninc/data/marketcore/domain/auth/JwtRequest.java
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/domain/auth/JwtRequest.java
@@ -5,10 +5,13 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class JwtRequest {
+public class JwtRequest implements Serializable {
+    private static final long serialVersionUID = 263011851808996064L;
     private String username;
     private String password;
 }
--- market-core/src/main/java/co/kr/pcninc/data/marketcore/domain/auth/JwtResponse.java
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/domain/auth/JwtResponse.java
@@ -1,17 +1,15 @@
 package co.kr.pcninc.data.marketcore.domain.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.io.Serializable;
 
+@Getter
+@AllArgsConstructor
 public class JwtResponse implements Serializable {
     private static final long serialVersionUID = -8091879091924046844L;
 
     private final String token;
-
-    public JwtResponse(String token) {
-        this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
+    private final String userId;
 }
