ea2ed01023ad0ddbd02ac9be52871dbad7965905
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:51:06 +0900

    spring boot initializer

---
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/ServletInitializer.java
@@ -0,0 +1,13 @@
+package co.kr.pcninc.data.marketcore;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MarketCoreApplication.class);
+	}
+
+}
