a55ff3421e5f3126c1bee52bd95f882b769419eb
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:50:31 +0900

    구입목록 service 정의

---
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/service/PurchaseListService.java
@@ -0,0 +1,24 @@
+package co.kr.pcninc.data.marketcore.service;
+
+import co.kr.pcninc.data.marketcore.domain.PurchaseList;
+import co.kr.pcninc.data.marketcore.repository.PurchaseListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PurchaseListService {
+
+    @Autowired
+    PurchaseListRepository purchaseListRepository;
+
+    public Page<PurchaseList> getListPageableY(String user, Pageable pageable) { return purchaseListRepository.findByUserY(user, pageable);}
+
+    public Page<PurchaseList> getListPageableN(String user, Pageable pageable) { return purchaseListRepository.findByUserN(user, pageable);}
+
+    public void createPurchase(PurchaseList purchaseList) { purchaseListRepository.save(purchaseList); }
+
+}
