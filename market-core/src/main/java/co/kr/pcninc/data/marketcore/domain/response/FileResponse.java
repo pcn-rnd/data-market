b3e87eb067d961cbedc3de3a39e6d60f33b950d1
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:49:38 +0900

    File Response 정의

--- market-core/src/main/java/co/kr/pcninc/data/marketcore/domain/response/FileResponse.java
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/domain/response/FileResponse.java
@@ -1,4 +1,10 @@
 package co.kr.pcninc.data.marketcore.domain.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class FileResponse {
+    private String savePath;
 }
