1e25db95cac7824a6bc7c4207c157b92f8c15c3b
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:44:47 +0900

    공통 using string 정의

---
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/common/Constants.java
@@ -0,0 +1,13 @@
+package co.kr.pcninc.data.marketcore.common;
+
+public class Constants {
+    public static final String FAILURE = "failure";
+    public static final String SUCCESS = "success";
+    public static final String ERROR = "error";
+    public static final String FATAL = "fatal";
+
+    public static final String DEFAULT_CHARSET_VALUE = "UTF-8";
+
+    public static final String API_ACCESS_TOKEN_ATTRIBUTE_NAME = "Authorization";
+    public static final String API_ACCESS_TOKEN_PREFIX = "Bearer";
+}
