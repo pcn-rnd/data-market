a4f1e4f1672fa0d58613ad78740328829acad61c
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:47:13 +0900

    구입목록 domain 정의

---
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/domain/PurchaseList.java
@@ -0,0 +1,40 @@
+package co.kr.pcninc.data.marketcore.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "DM_PURCHASE_LIST")
+public class PurchaseList {
+
+    @Id
+    @Column(name = "list_id")
+    @GeneratedValue
+    private int listId;
+
+    @ManyToOne
+    @JoinColumn(name = "set_id")
+    private Dataset setId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer")
+    private User customer;
+
+    //@Column(name = "pur_dt")
+    //private String purDt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date purDt;
+
+    @Column(name = "able_yn")
+    private String ableYn;
+}
