435e0ed7641c048b8ece84b37a0177515e43f1a0
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:40:59 +0900

    현재 사용자 관련 이슈 해결

--- market-core/src/main/java/co/kr/pcninc/data/marketcore/api/AuthRestController.java
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/api/AuthRestController.java
@@ -2,8 +2,10 @@ package co.kr.pcninc.data.marketcore.api;
 
 import co.kr.pcninc.data.marketcore.configuration.authorization.JwtTokenUtil;
 import co.kr.pcninc.data.marketcore.configuration.authorization.JwtUserDetailsService;
+import co.kr.pcninc.data.marketcore.domain.User;
 import co.kr.pcninc.data.marketcore.domain.auth.JwtRequest;
 import co.kr.pcninc.data.marketcore.domain.auth.JwtResponse;
+import co.kr.pcninc.data.marketcore.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +13,17 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
@@ -27,26 +38,59 @@ public class AuthRestController extends RestControllerBase{
     private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
-    private JwtUserDetailsService jwtUserDetailsService;
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity<?> login(@RequestBody JwtRequest request) {
-        authenticate(request.getUsername(), request.getPassword());
+    public ResponseEntity<?> login(@Valid @RequestBody JwtRequest request) {
+        Authentication authentication = authenticate(request.getUsername(), request.getPassword());
+        final String token = jwtTokenUtil.generateToken(authentication);
 
-        final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(request.getUsername());
-        final String token = jwtTokenUtil.generateToken(userDetails);
-
-        return okResponse(new JwtResponse(token));
+        return okResponse(new JwtResponse(token, request.getUsername()));
     }
 
-    private void authenticate(String username, String pwd) {
+    private Authentication authenticate(String username, String pwd) {
         try{
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, pwd));
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, pwd));
         }catch (DisabledException e) {
-
+            throw new DisabledException("USER_DISASBLED", e);
         }catch (BadCredentialsException e) {
-
+            throw new BadCredentialsException("INVALID_CREDENTIALS", e);
         }
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth =  SecurityContextHolder.getContext().getAuthentication();
+
+        if(auth != null && auth.isAuthenticated()) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return okResponse("로그아웃 완료");
+    }
+
+    @RequestMapping(value = "/current", method = RequestMethod.GET)
+    public ResponseEntity<?> getCurrentUser() {
+        Object authentication = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if(authentication.equals("anonymousUser")) {
+            return okResponse(null);
+        }
+
+        log.info("current user : {}", ((UserDetails) authentication).getUsername());
+        return okResponse(((UserDetails) authentication).getUsername());
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        user.setPwd(passwordEncoder.encode(user.getPwd()));
+        userService.saveUser(user);
+
+        return okResponse(user.getUserId());
     }
 
 }
