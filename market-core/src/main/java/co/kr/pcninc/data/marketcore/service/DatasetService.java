20d4dd2a96dfbeea7d6d46fce9d1b2fec1876730
pcn-rnd <snyang@pcninc.co.kr>
2021-10-05 09:50:21 +0900

    데이터셋 service 정의

--- market-core/src/main/java/co/kr/pcninc/data/marketcore/service/DatasetService.java
+++ market-core/src/main/java/co/kr/pcninc/data/marketcore/service/DatasetService.java
@@ -1,7 +1,6 @@
 package co.kr.pcninc.data.marketcore.service;
 
 import co.kr.pcninc.data.marketcore.domain.Dataset;
-import co.kr.pcninc.data.marketcore.domain.DatasetMapping;
 import co.kr.pcninc.data.marketcore.repository.DatasetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DatasetService {
@@ -20,16 +18,27 @@ public class DatasetService {
 
     public Page<Dataset> getDataSetList(Pageable page, String keyword) { return datasetRepository.findAllBy(page, keyword); }
 
+    public Page<Dataset> getDatasetListByCat(Pageable page, String keyword, int catId) { return datasetRepository.findAllByCat(page, keyword, catId); }
+
+
     public Dataset getDataSet(int setId) { return datasetRepository.findById(setId).orElse(null); }
 
     @Transactional
-    public void createDataset(Dataset dataset) {
+    public void save(Dataset dataset) {
         datasetRepository.save(dataset);
     }
 
     public void deleteDataset(int setId) { datasetRepository.deleteById(setId); }
 
-    public List<Dataset> getPopulars() { return datasetRepository.getPopulars(); }
+    public List<Dataset> getPopulars(int rows) { return datasetRepository.findPopulars(rows); }
 
-    public List<Dataset> getNewest() { return datasetRepository.getNewest(); }
+    public List<Dataset> getNewest() { return datasetRepository.findTop5ByOrderByCrtDtDesc(); }
+
+    public List<Dataset> getMostPurchased() { return datasetRepository.findMostPurchased(); }
+
+    public List<Object> getCountCats() { return datasetRepository.getCountCats(); }
+
+    public long getAllCounts() { return datasetRepository.count(); }
+
+    public long getYCnt(String publicYn) { return datasetRepository.countByPublicYn(publicYn); }
 }
