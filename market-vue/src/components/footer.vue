3e1a6446b81d915201066e0965e3294af0eee3c0
pcn-rnd <snyang@pcninc.co.kr>
2021-10-06 14:04:09 +0900

    footer

---
+++ market-vue/src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6">
+          {{ new Date().getFullYear() }} ©
+          <img class="ml-2 mr-2" style="width: 6%" src="../assets/images/companies/pcn.png"/>
+          <img class="mr-2" style="width: 8%" src="../assets/images/companies/dship.png"/>
+          <img class="mr-2" style="width: 10%" src="../assets/images/companies/sejong.png"/>
+        </div>
+        <!--<div class="col-sm-6">{{ new Date().getFullYear() }} © Skote.</div>-->
+        <div class="col-sm-6">
+          <!--<div class="text-sm-right d-none d-sm-block">Design & Develop by Themesbrand</div>-->
+          <div class="text-sm-right d-none d-sm-block">증강분석 빅데이터 프로파일링 데이터 마켓 시스템</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- end footer -->
+</template>
